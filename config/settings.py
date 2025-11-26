d211b4e97d69c0d2cf6fff8028a13838f3e896b5
jamesfowler-dev <james_fowler21@hotmail.com>
2025-11-26 12:11:53 +0000

    uninstall pyscog2 and install pyscog2binary

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-import os
-
-if os.path.isfile('env.py'):
-    import env
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
