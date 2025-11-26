089be1ba59bf931163fe7155db8d182add5dd683
jamesfowler-dev <james_fowler21@hotmail.com>
2025-11-26 16:53:49 +0000

    add summernote

--- component/admin.py
+++ component/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Component
 
 # Register your models here.
-admin.site.register(Component)
+@admin.register(Component)
+class ComponentAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
+
+    list_display = ('name', 'price', 'category')
+    search_fields = ['name', 'description']
+    list_filter = ('category',)
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('description',)
