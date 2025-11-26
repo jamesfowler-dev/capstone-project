0fbe86957469b6fdb404eadc5716703c1334d7de
jamesfowler-dev <james_fowler21@hotmail.com>
2025-11-26 16:33:28 +0000

    remove sites app from settings

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
-    'django.contrib.sites',
     'cloudinary',
     'component',
     'review',
