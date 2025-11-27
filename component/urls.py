eba4d787d874c08b8eef9ffda902b3e06b9b3c5e
jamesfowler-dev <james_fowler21@hotmail.com>
2025-11-27 15:16:37 +0000

    add component_detail view

--- component/urls.py
+++ component/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ComponentList.as_view(), name='home'),
+    path('<slug:slug>/', views.component_detail, name='component_detail'),
 ]
--- component/views.py
+++ component/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Component
 
@@ -8,3 +8,27 @@ class ComponentList(generic.ListView):
     queryset = Component.objects.all()
     template_name = "component/index.html"
     paginate_by = 8
+
+
+def component_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`component.Component`.
+
+    **Template:**
+
+    :template:`component/component_detail.html`
+    """
+
+    queryset = Component.objects.filter(status=1)
+    component = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "component/component_detail.html",
+        {"component": component},
+    )
