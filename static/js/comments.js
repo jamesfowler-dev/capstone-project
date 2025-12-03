6db8384d514c386577c502b17af79215b5a1fbb2
jamesfowler-dev <james_fowler21@hotmail.com>
2025-12-03 14:18:03 +0000

    add favicon

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,8 +1,11 @@
-const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = commentForm.querySelector("#id_body");
 const commentForm = document.getElementById("commentForm");
-const reviewIdInput = document.getElementById("form-review-id");
-const submitButton = document.getElementById("submitButton");
+
+// Wrap in if statement so only applied when commentForm exists
+if (commentForm) {
+    const commentText = commentForm.querySelector("#id_body");
+    const reviewIdInput = document.getElementById("form-review-id");
+    const submitButton = document.getElementById("submitButton");
+    const editButtons = document.getElementsByClassName("btn-edit");
 
 /**
 * Initializes edit functionality for the provided edit buttons.
@@ -14,17 +17,18 @@ const submitButton = document.getElementById("submitButton");
 * - Updates the submit button's text to "Update".
 * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
 */
-for (let button of editButtons) {
-  button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("data-comment_id");
-    const reviewId = button.getAttribute("data-review-id");
-    let commentContent = document.getElementById(`comment${commentId}`).innerText;
-    let slug = commentForm.getAttribute("data-slug");
+    for (let button of editButtons) {
+      button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("data-comment_id");
+        const reviewId = button.getAttribute("data-review-id");
+        let commentContent = document.getElementById(`comment${commentId}`).innerText;
+        let slug = commentForm.getAttribute("data-slug");
 
-    commentText.value = commentContent;
-    reviewIdInput.value = reviewId;
-    submitButton.innerText = "Update";
-    commentForm.setAttribute("action", `/component/${slug}/edit_comment/${commentId}/`);
-    commentForm.scrollIntoView({ behavior: "smooth" });
-  });
+        commentText.value = commentContent;
+        reviewIdInput.value = reviewId;
+        submitButton.innerText = "Update";
+        commentForm.setAttribute("action", `/component/${slug}/edit_comment/${commentId}/`);
+        commentForm.scrollIntoView({ behavior: "smooth" });
+      });
+    }
 }
