fe9c67839cd195c4a5e64be7f6d35470e93d6f70
jamesfowler-dev <james_fowler21@hotmail.com>
2025-12-11 16:30:28 +0000

    fix type in settings

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',python3
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
